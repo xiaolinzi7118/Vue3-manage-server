d1ced415740f542111bcbe2408644551b805f5e7
xiaolinzi7118 <1106147743@qq.com>
2021-07-18 16:27:21 +0800

    JWT添加token验证

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const logger = require('koa-logger')
 const log4js = require('./utils/log4j')
 const users = require('./routes/users')
 const router = require('koa-router')()
+const jwt = require('jsonwebtoken')
+const koajwt = require('koa-jwt')
+const util = require('./utils/util')
 
 // error handler
 onerror(app)
@@ -30,8 +33,22 @@ app.use(views(__dirname + '/views', {
 app.use(async (ctx, next) => {
   log4js.info(`get params:${JSON.stringify(ctx.request.query)}`)
   log4js.info(`post params:${JSON.stringify(ctx.request.body)}`)
-  await next()
+  await next().catch((err) => {
+    if (err.status == '401') {
+      //token过期中间件会把状态码设为401
+      ctx.status = 200;
+      ctx.body = util.fail('Token认证失败', util.CODE.AUTH_ERROR)
+    } else {
+      throw err;
+    }
+  })
 })
+
+//中间件
+//任何接口过来都会先校验token是否有效，unless里放白名单：不需要校验的请求
+app.use(koajwt({ secret: 'imooc' }).unless({
+  path: [/^\/api\/users\/login/]
+}))
 
 //routes
 
@@ -43,6 +60,13 @@ router.use(users.routes(), users.allowedMethods())
 //全局
 app.use(router.routes(), router.allowedMethods())
 
+// //请求/leave/count，验证token
+// router.get('/leave/count', (ctx) => {
+//   console.log('=>', ctx.request.headers)
+//   const token = ctx.request.headers.authorization.split(' ')[1];
+//   const payload = jwt.verify(token, 'imooc')
+//   ctx.body = payload;
+// })
 
 // error-handling
 app.on('error', (err, ctx) => {
--- routes/users.js
+++ routes/users.js
@@ -4,18 +4,33 @@
 const router = require('koa-router')()
 const User = require('./../models/userSchema')
 const util = require('./../utils/util')
+const jwt = require('jsonwebtoken')
 router.prefix('/users')
 
 router.post('/login', async (ctx) => {
   try {
     const { userName, userPwd } = ctx.request.body;
-    //去数据库查询
+    /**
+     * 返回数据库指定字段，有三种方式
+     * 1. 'userId userName userEmail state role deptId roleList'
+     * 2. {userId:1,_id:0}
+     * 3. select('userId')
+     */
     const res = await User.findOne({
       userName,
       userPwd
-    })
+    }, 'userId userName userEmail state role deptId roleList')
+
+    const data = res._doc;
+    const token = jwt.sign({
+      data: data
+    }, 'imooc', { expiresIn: '1h' })
+
+    console.log('token=>', token);
+
     if (res) {
-      ctx.body = util.success(res)
+      data.token = token;
+      ctx.body = util.success(data)
     } else {
       ctx.body = util.fail("账号或密码不正确")
     }
